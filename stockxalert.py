b32bd8d65800e743f81a3940b9630b07817ae66a
algreen11 <AlexT@Alexs-MBP.lan>
2017-07-30 16:36:30 -0700

    removed user set of current_price and added prettyprint

--- stockxalert.py
+++ stockxalert.py
@@ -4,16 +4,17 @@ from bs4 import BeautifulSoup
 from twilio.rest import Client
 import time
 
+### SET USER INFORMATION ## 
 account_sid = "TWILIO ACCOUNT SID"
 auth_token = "TWILIO ACCOUNT AUTH TOKEN"
 twilio_phone_number = 'TWILIO NUMBER'
 user_phone = 'MY PHONE NUMBER'
-
 shoe_url = 'https://stockx.com/adidas-yeezy-boost-350-pirate-black-2016'
 size = 10
 price_threshold = 1200
-current_price = 1250
 refresh_minutes = 60
+###########################
+current_price = 0
 
 def Scrape(soup):
 	shoesize, price = ([] for i in range(2))
@@ -28,6 +29,12 @@ def Scrape(soup):
 	x = 0
 	while x < len(shoe):
 		dic[shoe[x]] = pri[x]
+		try:
+			if float(shoe[x]) == float(size):
+				global current_price
+				current_price = int(pri[x][1:])
+		except:
+			pass
 		x += 1
 	return dic
 
@@ -35,7 +42,7 @@ def CheckPrice(price_dict):
 	new = price_dict.get(str(size))
 	new = int(new[1:])
 	global current_price
-	if new < current_price and new < price_threshold:
+	if new <= current_price and new < price_threshold:
 		current_price = new
 		return True
 	else: 
@@ -46,15 +53,22 @@ def Text(message):
 	client = Client(account_sid, auth_token)
 	client.messages.create(to=user_phone, from_=twilio_phone_number, body=message)
 
+def PrettyPrint(message):
+	output = ''
+	split = message.split('-')
+	for x in split:
+		output += x.capitalize() + ' '
+	return output
+
 def main():
 	while True:
 		request = urllib.request.urlopen(shoe_url).read()
 		soup = BeautifulSoup(request, "html.parser")
 		shoe_data = Scrape(soup)
 		if CheckPrice(shoe_data):
-			Text('The ' + shoe_url[19:] + ' have dropped below $' + str(price_threshold) + 
-				'. They are now $' + str(current_price) + '. Check it out at ' + shoe_url)
-			# print('The ' + shoe_url[19:] + ' have dropped below $' + str(price_threshold) + '. They are now $' + str(current_price) + '. Check it out at ' + shoe_url)
+			# Text('The ' + PrettyPrint(shoe_url[19:]) + 'have dropped below $' + str(price_threshold) + 
+			# 	'. They are now $' + str(current_price) + '. Check it out at ' + shoe_url)
+			print('The ' + PrettyPrint(shoe_url[19:]) + 'have dropped below $' + str(price_threshold) + '. They are now $' + str(current_price) + '. Check it out at ' + shoe_url)
 		time.sleep(60*refresh_minutes)
 
 if __name__ == '__main__':
